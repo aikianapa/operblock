d3a6caabebfaa78d474bd7a5bf0d7e82479da837
Oleg Frolov <oleg_frolov@mail.ru>
2015-02-09 15:44:52 +0300

    calendar medicalAidType fix (2)

--- functions.php
+++ functions.php
@@ -635,6 +635,7 @@ while($data = mysql_fetch_array($res)) {$item=$data["name"];}
 return $item;
 }
 
+
 function getOperationsByPerson($month,$year,$person_id,$role="person_id") {
 		$start="$year-$month-01";
 		$stop="$year-$month-31";
@@ -644,7 +645,7 @@ $SQL="SELECT Action.id FROM Action
 	INNER JOIN Event ON Action.event_id = Event.id
 	INNER JOIN EventType ON Event.EventType_id = EventType.id
 	WHERE ActionType.serviceType = 4
-	AND EventType.medicalAidType_id < 5 
+	AND EventType.medicalAidType_id < 4 
 	AND ( Action.begDate BETWEEN '$start 00:00:00' AND '$stop 23:59:59' OR (Action.plannedEndDate BETWEEN '$start 00:00:00' AND '$stop 23:00:59'  ) )
 	AND Action.deleted=0
 	ORDER BY Action.status DESC ";
@@ -686,7 +687,7 @@ $SQL="SELECT Action.id FROM Action
 	INNER JOIN Event ON Action.event_id = Event.id
 	INNER JOIN EventType ON Event.EventType_id = EventType.id
 	WHERE ActionType.serviceType = 4 and Person.orgStructure_id=$oid
-	AND EventType.medicalAidType_id < 5 
+	AND EventType.medicalAidType_id < 4 
 	AND Action.deleted = 0 
 	AND ( Action.begDate BETWEEN '$start 00:00:00' AND '$stop 23:59:59' OR (Action.plannedEndDate BETWEEN '$start 00:00:00' AND '$stop 23:00:59'  ) )
   ORDER BY Action.status DESC ";
@@ -696,7 +697,7 @@ $SQL="SELECT Action.id FROM Action
 		INNER JOIN Event ON Action.event_id = Event.id
 		INNER JOIN EventType ON Event.EventType_id = EventType.id
 		WHERE ActionType.serviceType = 4
-		AND EventType.medicalAidType_id < 5  
+		AND EventType.medicalAidType_id < 4  
 		AND ( Action.begDate BETWEEN '$start 00:00:00' AND '$stop 23:59:59' OR (Action.plannedEndDate BETWEEN '$start 00:00:00' AND '$stop 23:00:59'  ) )
 		ORDER BY status DESC ";
 }
