30a1df26647b71e7712c552d5de1a722014c5b3e
Oleg Frolov <oleg_frolov@mail.ru>
2015-06-17 17:16:17 +0300

    Update

--- forms/epicrizOut.php
+++ forms/epicrizOut.php
@@ -337,9 +337,9 @@ function fields_msk36($event_id,$orgstr="") {
 			$f["e_stateIn"]=implode(", ",$f["e_stateIn"]); if ($f["e_stateIn"]=="Периферические отеки: ") {$f["e_stateIn"]="";}
 
 			$f["e_stateView"]=array(); // Состояние при осмотре
-				if ($docs[""]["Состояние при осмотре:"]["value"]>"") $f["e_stateView"][]=$["Состояние при поступлении:"]["value"];
-				if ($docs[""]["Тяжесть состояния обусловлена"]["value"]>"") $f["e_stateView"][]=$["Тяжесть состояния обусловлена"]["value"];
-				if ($docs[""]["Телосложение:"]["value"]>"") $f["e_stateView"][]=$["Телосложение:"]["value"];
+				if ($docs["DiaryLast"]["Состояние при осмотре:"]["value"]>"") $f["e_stateView"][]=$["Состояние при поступлении:"]["value"];
+				if ($docs["DiaryLast"]["Тяжесть состояния обусловлена"]["value"]>"") $f["e_stateView"][]=$["Тяжесть состояния обусловлена"]["value"];
+				if ($docs["DiaryLast"]["Телосложение:"]["value"]>"") $f["e_stateView"][]=$["Телосложение:"]["value"];
 				$f["e_stateView"][]="Периферические отеки: ".getTextFromAction($,"Периферические отеки:","Дизурические явления:");
 			$f["e_stateView"]=implode(", ",$f["e_stateView"]); if ($f["e_stateView"]=="Периферические отеки: ") {$f["e_stateView"]="";}
 
